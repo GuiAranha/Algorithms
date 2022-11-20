74d8a5625fe6e8e293700deebad482ddc6df95a9
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-11-20 11:45:51 -0300

    feat: create study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        counter = 0
+        for hour in permanence_period:
+            entry, exit = hour
+            if target_time >= entry and target_time <= exit:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
