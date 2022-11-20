4802dcb376c0ca4792ed3bad7b5dc42b6d59e0ae
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-11-20 11:59:24 -0300

    feat: create test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    encrypt_1 = encrypt_message("AABBCC", 3)
+    assert (encrypt_1) == "BAA_CCB"
+
+    encrypt_1 = encrypt_message("AABBCC", 4)
+    assert (encrypt_1) == "CC_BBAA"
+
+    encrypt_1 = encrypt_message("AABBCC", -1)
+    assert (encrypt_1) == "CCBBAA"
+
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+    with pytest.raises(TypeError):
+        encrypt_message("teste", "")
